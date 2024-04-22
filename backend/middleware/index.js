8cee53819dda525264e2b4b1c41ed6af7fbf0d0c
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-22 09:21:04 +0500

    Added middleware

---
+++ backend/middleware/index.js
@@ -0,0 +1,22 @@
+const { Admin } = require("../db");
+const { jwtSecret } = require('../config')
+var jwt = require('jsonwebtoken');
+
+function adminMiddleware(req, res, next) {
+
+    let token = req.headers.authorization;
+   
+    if (token){
+        jwt.verify(token,jwtSecret,(err,user)=>{
+            if (err){
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = adminMiddleware;
