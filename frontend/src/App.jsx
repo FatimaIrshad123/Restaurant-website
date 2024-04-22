d91ab580c7006db41c0d263a7f844064d885cbc1
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-22 11:17:08 +0500

    Updated login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import {
 } from "react-router-dom";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
+import Signup from "./components/Signup";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/login" element={<Login />}/>
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
       
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -26,7 +26,7 @@ export default function Login() {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 ">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
+                  Login to your account
               </h1>
         </div>
         <div className="justify-center m-5">
@@ -34,20 +34,20 @@ export default function Login() {
             <br/>
             <input type="text" 
             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            value={username} placeholder="Username" onChange={e => setUsername(e.target.value)}/>
+            value={username} placeholder="Enter Username" onChange={e => setUsername(e.target.value)}/>
             <br/>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
             <br/>
             <input type="password" 
             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={password} placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+            value={password} placeholder="Enter Password" onChange={e => setPassword(e.target.value)}/>
             <br/>
           <button 
           className="w-full text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
          
               onClick={sendRequest}
               >
-                Sign in</button>
+                Login</button>
                 
                 </div>    
                 <p class="text-sm font-light 
