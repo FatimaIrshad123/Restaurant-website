439b34ee41390843f3ecb7309ca1e59cf104a337
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-26 17:34:50 +0500

    Updated code

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import Navbar from "./Navbar"
 import { URL } from "../url"
+import React from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Cart({id}){
     const [cart,setCart] = useState([])
     const [total,setTotal] = useState()
 
-    
+    const navigate = useNavigate()
     let sum = [];
     let num = 0
    useEffect (() => {
@@ -15,23 +17,27 @@ export default function Cart({id}){
         axios.get(`${URL}/admin/all/ordered`)
         .then((res) => {return res.data})
         .then(res => {setCart(res)})
-    },5000)
+    },100)
     return () => clearInterval(interval) 
    },[])
+
    useEffect(() => {
-    const interval = setInterval(() => {
+    try{
         axios.get(`${URL}/admin/all/ordered`)
         .then((res) => {return res.data})
         .then((res) => res.map((e) => {
          let x = e.price
          sum.push(x)
          const sum1 = sum.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-         setTotal(sum1)
+         React.memo(setTotal(sum1))
          }))
-    },5000)
-    return () => clearInterval(interval) 
-   },[total])
-   console.log(total)
+    }catch(error){
+        console.log('error',error)
+    }
+        
+         //console.log(total)
+   },[])
+  
 
    
     return (
@@ -56,7 +62,8 @@ export default function Cart({id}){
                                 await axios.post(`https://resturant-website-bd3aac525b4d.herokuapp.com/admin/delete`,{
                                     id:e._id})
                                     alert('order deleted successfully')
-                            }}>
+                                    navigate('/')
+                            }} className="pl-10">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                 </svg>
@@ -69,9 +76,11 @@ export default function Cart({id}){
             <div>
             <div className="bg-gray-100 pb-5 rounded-lg max-w-screen-lg mx-5">
                 <h1 className="text-4xl text-left py-5 bg-white">Total</h1>
-                <div className="flex font-bold p-3 rounded w-screen">
+                <div className="flex font-bold 
+                p-3 rounded w-screen">
+                     <h2 className="pl-20">Your bill: </h2>
                     <h2 className="pr-48">{total}</h2>
-                    <h2 className="pl-20">Price</h2>
+                   
                 </div>
             </div>
             </div>
