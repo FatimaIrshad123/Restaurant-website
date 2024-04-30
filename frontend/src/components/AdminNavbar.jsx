14982bc03208574286835c3fe42a1c6b9b06bb09
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-04-30 13:23:45 +0500

    Updated frontend

--- frontend/src/components/AdminNavbar.jsx
+++ frontend/src/components/AdminNavbar.jsx
@@ -5,19 +5,16 @@ import { AiOutlineMenuUnfold } from "react-icons/ai"
 import { useState } from "react"
 
 export default function AdminNavbar(){
-    const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
   const handleChange = () => {setMenu(!menu);};
   const closeMenu = () => {setMenu(false);};
 
-    return (
-        <div>
-            <div className=" fixed w-full">
-      <div>
+  return (
+    <div>
+      <div className=" fixed w-full">
         <div className=" flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <div className=" flex flex-row items-center cursor-pointer">
-            <span>
-              <BiRestaurant size={32} />
-            </span>
+            <span><BiRestaurant size={32} /> </span>
             <h1 className=" text-xl font-semibold">FoodieWeb</h1>
           </div>
           <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
@@ -34,7 +31,6 @@ export default function AdminNavbar(){
               className="hover:text-brightColor transition-all cursor-pointer">Signup 
             </NavLink>
           </nav>
-
           <div className="md:hidden flex items-center">
             {menu ? (
               <AiOutlineClose size={25} onClick={handleChange} />
@@ -42,7 +38,6 @@ export default function AdminNavbar(){
               <AiOutlineMenuUnfold size={25} onClick={handleChange} />
             )}
           </div>
-        </div>
         <div
           className={` ${
             menu ? "translate-x-0" : "-translate-x-full"
